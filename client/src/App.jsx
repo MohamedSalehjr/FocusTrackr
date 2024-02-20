485bc331c036fc9e819dba0af57e88289d5e4230
Mohamed  Saleh <mohamedsalehjr@gmail.com>
2024-02-20 10:46:23 +0000

    project sync

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 import {
   ClerkProvider,
+  useUser,
   SignedIn,
   SignedOut,
   RedirectToSignIn,
--- client/src/components/Report.jsx
+++ client/src/components/Report.jsx
@@ -3,11 +3,9 @@ import { useState, useEffect } from "react";
 import "react-calendar-heatmap/dist/styles.css";
 import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip } from "react-tooltip";
-// import "../../styles/style.css"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -15,9 +13,7 @@ import {
 
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "../../components/ui/card";
@@ -25,7 +21,9 @@ import {
 const Report = () => {
   const current = new Date();
   let extractedData;
+  let extractedTotalTime;
   const [data, setData] = useState(null);
+  const [totalTime, setTotalTime] = useState(0);
 
   useEffect(() => {
     // Fetch operation
@@ -37,23 +35,19 @@ const Report = () => {
       } catch (error) {
         console.error("Error fetching data:", error);
       }
-      // console.log(data);
+
+      try {
+        const response = await fetch(
+          "http://localhost:4000/api/users/user_2bNEmunwXIIVut4RrIFg5EqDRHI"
+        );
+        const time = await response.json();
+        setTotalTime(time);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
     fetchData(); // Call the fetch function
-    // if (data != null) {
-    // }
-    // const extractedData = data.pomo.map((item) => {
-    //   return {
-    //     date: item.date,
-    //     count: item.count,
-    //   };
-    //   console.log(extractedData);
-
-    // });
-
-    // console.log(extractedData);
-    // Cleanup function (not needed here since we're not subscribing to any resources)
   }, []);
 
   if (!data) {
@@ -61,16 +55,6 @@ const Report = () => {
   } else if (data.error) {
     // handle error
   } else {
-    // process results
-    // console.log(
-    //   data.pomo.map((item) => {
-    //     return {
-    //       date: item.date,
-    //       count: item.count,
-    //     };
-    //   })
-    // );
-
     extractedData = data.pomo.map((item) => {
       return {
         date: item.date,
@@ -78,7 +62,13 @@ const Report = () => {
       };
     });
 
-    console.log(extractedData[0]);
+    // console.log(extractedData[0]);
+  }
+
+  if (!totalTime) {
+  } else if (totalTime.error) {
+  } else {
+    extractedTotalTime = totalTime.user.hours;
   }
 
   const today = `${current.getFullYear()}-${
@@ -94,7 +84,9 @@ const Report = () => {
         <DialogContent>
           <Card className="w-1/2 mx-auto">
             <CardHeader className="">
-              <CardTitle className="self-center text-5xl">23 hrs</CardTitle>
+              <CardTitle className="self-center text-5xl">
+                {extractedTotalTime}
+              </CardTitle>
               <CardDescription className="self-center">
                 Total Time focused
               </CardDescription>
