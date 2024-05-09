7803700151b492b0a0d6fccb1270d3f44c110d89
mohamedsalehjr <mohamedsalehjr@gmail.com>
2024-05-09 20:13:54 +0100

    adding todos to local storage

--- client/src/components/TodoWrapper.jsx
+++ client/src/components/TodoWrapper.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TodoForm from "./TodoForm"
-import {useState, useEffect} from 'react'
-import {v4 as uuidv4} from 'uuid'
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Todo from './Todo'
 
 
@@ -9,35 +9,37 @@ uuidv4();
 
 const TodoWrapper = () => {
 
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem('todos')
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  })
 
-    const addTodo = todo => {
 
-        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, }])
-        console.log(todos)
-    }
+  const addTodo = todo => {
+    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, }])
+  }
 
-    const toggleComplete = (id) => {
-        setTodos(todos.map(todo => todo.id === id ? {...todo, completed : !todo.completed} : todo) )
-    }
-    const deleteTodo = (id) => {
-        setTodos(todos.filter(todo => todo.id !== id))
-    }
+  const toggleComplete = (id) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+  }
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
 
-    useEffect(() => {
-        localStorage.setItem('dataKey', JSON.stringify(todos));
-      }, [todos]);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+
   return (
     <div className="TodoWrapper mt-10">
-
-        <TodoForm addTodo={addTodo}/>
-        {todos.map((todo,index) => (
-            <Todo task={todo} key={index}
-            toggleComplete={toggleComplete}
-            deleteTodo={deleteTodo}
-            />
-        ))}
-
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
+      ))}
     </div>
   )
 }
